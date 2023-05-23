573a6fa629cd91fefc6e4123e063fb3e28ae339b
Kevin-Kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-05-23 23:09:32 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  let tasks = TASKS;
+  const [list, setList] = useState(tasks);
+  const [deletedList, setDeleted] = useState(tasks);
+
+  const categories = CATEGORIES;
+
+  function deleted(task) {
+    setList((lastValue) => lastValue.filter((el) => el !== task));
+    setDeleted((lastValue) => lastValue.filter((el) => el !== task));
+  }
+
+  function selected(category) {
+    categories.map((cat) => {
+      cat !== category
+        ? document.querySelector(`.${cat}`).classList.remove("selected")
+        : document.querySelector(`.${category}`).classList.add("selected");
+      setList(() =>
+        deletedList.filter((task) =>
+          category === "All" ? true : task.category === category
+        )
+      );
+    });
+  }
+
+  function onTaskFormSubmit(newObj) {
+    setList((lastValue) => {
+      setDeleted((lastValue) => [...lastValue, newObj]);
+      return [...lastValue, newObj];
+    });
+    return newObj;
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={categories} click={selected} />
+      <NewTaskForm
+        categories={CATEGORIES}
+        onTaskFormSubmit={onTaskFormSubmit}
+      />
+      <TaskList tasks={list} click={deleted} />
     </div>
   );
 }
